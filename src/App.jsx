903d8268b2ef1e9c7c125d680103f99574ae490d
Tushar <tusharchoudhary7070@gmail.com>
2026-02-24 05:13:44 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Suspense, lazy } from "react";
 import Layout from "./layout/Layout";
-import Loader from "./components/Loader";
-
-// Lazy Imports
-const Home = lazy(() => import("./pages/Home"));
-const Portfolio = lazy(() => import("./pages/Portfolio"));
-const PortfolioDetail = lazy(() => import("./pages/PortfolioDetail"));
-const About = lazy(() => import("./pages/About"));
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import PortfolioDetail from "./pages/PortfolioDetail";
+import About from "./pages/About";
 
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="portfolio" element={<Portfolio />} />
-            <Route path="portfolio/:id" element={<PortfolioDetail />} />
-            <Route path="about" element={<About />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="portfolio/:id" element={<PortfolioDetail />} />
+          <Route path="about" element={<About />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
