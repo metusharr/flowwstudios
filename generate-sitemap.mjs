925160584274eddf6e397be803c80a50aa6559f4
Tushar <tusharchoudhary7070@gmail.com>
2026-08-06 15:59:39 +0530

    changes

--- generate-sitemap.mjs
+++ generate-sitemap.mjs
@@ -9,26 +9,32 @@ const writeStream = createWriteStream("./public/sitemap.xml");
 
 sitemap.pipe(writeStream);
 
+const lastmod = new Date().toISOString();
+
 const routes = [
   {
     url: "/",
     changefreq: "weekly",
     priority: 1.0,
+    lastmod,
   },
   {
     url: "/about",
     changefreq: "monthly",
     priority: 0.8,
+    lastmod,
   },
   {
     url: "/portfolio",
     changefreq: "weekly",
     priority: 0.9,
+    lastmod,
   },
   {
     url: "/contact",
     changefreq: "monthly",
     priority: 0.8,
+    lastmod,
   },
 ];
 
